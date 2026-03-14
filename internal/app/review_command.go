b60e666958190fcf8c2e38f762c1018599f3ef24
El Mehdi Tabet <el-mehdi.tabet.1@ens.etsmtl.ca>
2026-03-14 01:15:19 -0400

    add filesystem validation for review repository path

--- internal/app/review_command.go
+++ internal/app/review_command.go
@@ -1,6 +1,9 @@
 package app
 
-import "errors"
+import (
+	"errors"
+	"os"
+)
 
 type ReviewRequest struct {
 	RepoPath string
@@ -10,5 +13,14 @@ func RunReview(req ReviewRequest) (string, error) {
 	if req.RepoPath == "" {
 		return "", errors.New("repo path is required")
 	}
+	info, err := os.Stat(req.RepoPath)
+
+	if err != nil {
+		return "", errors.New("repo path does not exist: " + req.RepoPath)
+	}
+	if !info.IsDir() {
+		return "", errors.New("repo path must be a directory")
+	}
+
 	return "review command invoked for repo: " + req.RepoPath, nil
 }
